f0bc6d775ea69c02caf2773120ce2a4b67b21bbf
unparalleled-js <yingthi@live.com>
2021-11-29 09:05:47 -0600

    fix: rm py 36

--- setup.py
+++ setup.py
@@ -71,7 +71,6 @@ setup(
         "Operating System :: MacOS",
         "Operating System :: POSIX",
         "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.6",
         "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
