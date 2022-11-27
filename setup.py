23fbab9d1be10d361a4619107dcb4215acdc1295
unparalleled-js <yingthi@live.com>
2022-11-26 19:16:32 -0600

    chore: upgrades

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ extras_require = {
     ],
     "lint": [
         "black>=22.10.0",  # auto-formatter and linter
-        "mypy>=0.982",  # Static type analyzer
+        "mypy>=0.991",  # Static type analyzer
         "types-setuptools",  # Needed for mypy type shed
         "flake8>=5.0.4",  # Style linter
         "isort>=5.10.1",  # Import sorting linter
