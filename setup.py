24059a650c70b9e592b5d1a0d31081ab1fc2967c
NotPeopling2day <32708219+NotPeopling2day@users.noreply.github.com>
2023-11-07 20:25:27 +0100

    chore: update template

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ extras_require = {
         "hypothesis>=6.2.0,<7.0",  # Strategy-based fuzzer
     ],
     "lint": [
-        "black>=23.10.0,<24",  # Auto-formatter and linter
+        "black>=23.10.1,<24",  # Auto-formatter and linter
         "mypy>=1.6.1,<2",  # Static type analyzer
         "types-setuptools",  # Needed for mypy type shed
         "flake8>=6.1.0,<7",  # Style linter
@@ -18,6 +18,7 @@ extras_require = {
         "mdformat>=0.7.17",  # Auto-formatter for markdown
         "mdformat-gfm>=0.3.5",  # Needed for formatting GitHub-flavored markdown
         "mdformat-frontmatter>=0.4.1",  # Needed for frontmatters-style headers in issue templates
+        "mdformat-pyproject>=0.0.1",  # Allows configuring in pyproject.toml
     ],
     "release": [  # `release` GitHub Action job uses this
         "setuptools",  # Installation tool
