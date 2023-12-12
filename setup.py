d5bba17bea4b6a5de49af81bf596d742ede041b5
NotPeopling2day <32708219+NotPeopling2day@users.noreply.github.com>
2023-12-12 17:04:41 -0600

    chore: black update

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ extras_require = {
         "hypothesis>=6.2.0,<7.0",  # Strategy-based fuzzer
     ],
     "lint": [
-        "black>=23.11.0,<24",  # Auto-formatter and linter
+        "black>=23.12.0,<24",  # Auto-formatter and linter
         "mypy>=1.7.1,<2",  # Static type analyzer
         "types-setuptools",  # Needed for mypy type shed
         "types-requests",  # Needed for mypy type shed
